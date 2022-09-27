b46a8bc0ad7519307d4ca466dd8536f99514098a
Jafarammer <jafalkaff96@gmail.com>
2022-09-27 13:56:17 +0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+// MUI
+import { Box } from "@mui/material";
+
+function Navbar() {
+  return <Box>Navabar</Box>;
+}
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+// MUI
+import { Box } from "@mui/material";
+
+function Home() {
+  return <Box>HOme</Box>;
+}
+
+export default Home;
